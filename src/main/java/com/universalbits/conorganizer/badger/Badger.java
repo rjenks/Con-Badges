7bcb93a88185b3edf89eb524f4a8e18c343980cc
Robert Jenks <rjenks@universalbits.com>
2014-03-28 20:07:48 -0500

    Renamed main class to remove confusion and upped version number

--- src/main/java/com/universalbits/conorganizer/badger/Badger.java
+++ src/main/java/com/universalbits/conorganizer/badger/Badger.java
@@ -2,7 +2,7 @@ package com.universalbits.conorganizer.badger;
 
 import com.universalbits.conorganizer.badger.ui.BadgePrinterUI;
 
-public class BadgePrinter {
+public class Badger {
 
 	public static void main(String[] args) {
 		new BadgePrinterUI();
